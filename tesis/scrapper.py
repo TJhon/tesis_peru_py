dcb75180c439aa404162f2779853ec9cee0cf1d6
jhon <fr.jhonk@gmail.com>
2023-08-28 15:45:17 -0500

    done pucp

--- tesis/scrapper.py
+++ tesis/scrapper.py
@@ -5,17 +5,21 @@ import math
 from bs4 import BeautifulSoup as bs
 import urllib3
 from tqdm import tqdm
+import os
+import re
+
 urllib3.disable_warnings()
 
 
 
 from utils import *
 
-class Download:
+class ThesisPeru:
 	def __init__(
 		self, url, recent_str = 'recent-submissions?offset=', prefix_url = None, split_url = '/repositorio',
-		metadata_show = '?show=full', download_dir = 'thesis_download', replace_dshome = True
+		metadata_show = '?show=full', download_dir = 'thesis_download', replace_dshome = True, force_download = False
 		):
+
 		if prefix_url is None and split_url is not None:
 			prefix_url, _ = url.split(split_url)
 
@@ -25,12 +29,14 @@ class Download:
 		self.download_dir = download_dir
 		self.recent_str = recent_str
 		self.replace_str = replace_dshome
+		self.force_download = force_download
 	def read_html(self, url):
+		"""
+		read_html(url) | DRYS
+		"""
 		html_rq = rs.get(url, verify=False).content
 		html = bs(html_rq, 'html.parser')
 		return html
-	# 'hola'.t
-
 	def get_pages_url(self, n = 0, css = 'pagination-info'):
 
 		url_main = f'{self.url_collection}/{self.recent_str}{n}'
@@ -80,46 +86,161 @@ class Download:
 	def url_to_dir_path(self, html, css = 'breadcrumb hidden-xs'):
 		info = html.find('ul', class_=css)
 		info = info.find_all('a')
-		info = [a.text for a in info]
+		info = [tidy_text(a.text) for a in info]
+		info = [t.replace(' ', '_').replace('.', '_') for t in info]
+		info = [t.replace('__', '_').replace('_(', '_') for t in info]
+		info = [t.replace('_)', '') for t in info]
 		if self.replace_str:
 			info[0] = self.download_dir
-		return info
+		
+		if not os.path.exists(info[0]):
+			os.makedirs(info[0])
+		
+		full_dir = '/'.join(info)
+		if not os.path.exists(full_dir):
+			os.makedirs(full_dir)
+
+		return full_dir
+	def pdf_avaible(self, html, css = 'file-link') -> []:
+		links = html.find_all('div', class_=css)
+		links = [link.find('a')['href'] for link in links]
+
+		pdf_options = [re.search(r'=1|=3|=4|=5|=10|=9', item) is not None for item in links]
+		pdf_valid = [re.search(r'Allowed=y', item) is not None for item in links]
+		pdf_exists = [re.search(r'pdf', item) is not None for item in links]
+		pdf_resid = [
+			item 
+			for item, opt, val, ext in 
+			zip(links, pdf_options, pdf_valid, pdf_exists) 
+			if opt and val and ext
+		]
+
+		return pdf_resid
+	def download_pdf(self, links, to_dir, author_year):
+
+		def download_here(from_url, filename):
+			if self.force_download or not os.path.exists(filename):
+				# print('s')
+				try:
+					pdf = rs.get(from_url).content
+					with open(filename, 'wb') as f:
+						f.write(pdf)
+				except: pass
+		
+		ext = 'pdf'
+
+		pdf_avaible = False
+		link_show = None
+		n_link = len(links)
+
+		file_name = ''
+
+		if n_link > 1:
+			pdf_avaible = True
+			link_show = '\n'.join(links)
+			for i in range(n_link):
+				file_name_i = f'{to_dir}/{author_year}_{i}.{ext}'
+				file_name += file_name_i + '\n'
+				download_here(links[i], file_name)
+		elif n_link > 0:
+			pdf_avaible = True
+			link_show = links[0]
+			file_name = f'{to_dir}/{author_year}.{ext}'
+			download_here(link_show, file_name)
+
+		return pdf_avaible, link_show, file_name
+
+	def get_names_year(self, df):
+		try:
+			columns = ['dc.contributor.author', 'dc.date.created']
+			author, year = df[columns].to_numpy()[0]
+			# author_year = df[columns].to_numpy()[0]
+			author_name = first_2lether(author)
+			author_year = f'{author_name}_{year}'
+		except:
+			print('random_author')
+			author_year = random_author()
+		return author_year
 	def get_metadata(self, url):
 		thesis_html = self.read_html(url)
+
 		metadata = str(thesis_html.find('table'))
-		dir_ = self.url_to_dir_path(thesis_html)
-		# df = pd.read_html(metadata)[0][[0, 1]]
-		# df.columns = ['column', 'info']
-		return dir_
-		
 
-	# def 
-# /html/body/div[1]/div/div/div/ul
+		# make dir recursive
+		directory = self.url_to_dir_path(thesis_html)
+		links = self.pdf_avaible(thesis_html)
+		links = [self.prefix_url + link for link in links]
 
 
-url_collection = 'https://tesis.pucp.edu.pe/repositorio/handle/20.500.12404/6'
-# a = url_collection.split('/repositorio')
-a = Download(url_collection)
-b = a.get_pages_url().get_dict_page(1)
-pag_n = list(b.thesis_dict.keys())
+		df : pd.DataFrame= pd.read_html(str(metadata))[0][[0, 1]] 
+		df = df.groupby(0, as_index=False).agg("\n".join)
+		df = df.set_index(0).T.reset_index().drop(columns=["index"])
 
-thesi_dict = b.thesis_dict
-c = thesi_dict[pag_n[0]][0]
+		author_year = self.get_names_year(df)
 
-d = b.get_metadata(c)
-print(d)
+		pdf_avaible, link_show, local_file = self.download_pdf(links, directory, author_year)
+		df = df.assign(pdf_avaible = pdf_avaible, pdf_online = link_show, local_file = local_file, url_thesis = url)
 
-# print(c)
+		return  df
+	def save_metadata(self, data: pd.DataFrame, name: str, fail = False):
+		dir_main = self.download_dir
+		dir_metadata = f'{dir_main}/00metadata'
+		if fail:
+			dir_metadata += '/fail'
+		else:
+			dir_metadata += '/success'
 
-# thesis_dict = {}
-# thesis_dict['a'] = [12, 3]
-# thesis_dict['b'] = [12, 3]
-# thesis_dict['c'] = [12, 1]
-# b = pd.DataFrame(thesis_dict)
-# print(b)
-# print(a.get_tesis_from_page(page[1]))
+		if not os.path.exists(dir_metadata):
+			os.makedirs(dir_metadata)
+		data.to_json(f'{dir_metadata}/{name}.json')
+	def get_metadata_page(self, page = None):
+		thesis_dict = self.thesis_dict	
+		pages_n = list(thesis_dict.keys())
+		n_page = len(pages_n)
+		if page is not None:
+			n_page = page
 
-# print(a.prefix_url)
-# a
-# print(a)
-	# print(get_pages_url(url_collection))
+		all_metadata = pd.DataFrame()
+		all_errors = {}
+
+		for page in tqdm(range(n_page)):
+			all_metadata_in_page = pd.DataFrame()
+
+			all_errors_in_page = []
+			a_error = all_errors_in_page.append
+			thesis_in_page = thesis_dict[pages_n[page]]
+			for thesis in tqdm(thesis_in_page):
+				try:
+					metadata = self.get_metadata(thesis)
+					all_metadata_in_page = pd.concat([all_metadata_in_page , metadata])
+				except:
+					a_error(thesis)
+				
+
+			self.save_metadata(all_metadata_in_page.reset_index(), str(pages_n[page]))
+
+			try:
+				all_metadata = pd.concat([all_metadata, all_metadata_in_page])
+			except: 
+				all_errors[pages_n[page]] = all_errors_in_page
+			
+		try:
+			self.save_metadata(all_metadata.reset_index(), '00_all_pages_metadata')
+		except:
+			try:
+				errors_df = pd.DataFrame(all_errors)
+				self.save_metadata(errors_df, '00_all_pages_fail', True)
+			except:
+				pass
+			
+		return all_metadata, all_errors
+
+
+
+# url_collection = 'https://tesis.pucp.edu.pe/repositorio/handle/20.500.12404/6'
+# # a = url_collection.split('/repositorio')
+# a = ThesisPeru(url_collection)
+# b = a.get_pages_url().get_dict_page(2)
+
+# print('download files')
+# all_, error = b.get_metadata_page()
--- tesis/utils.py
+++ tesis/utils.py
@@ -3,22 +3,32 @@ import requests as rs
 import math
 from bs4 import BeautifulSoup as bs
 import urllib3
-from unidecode import unidecode
+import random, string
+# from unidecode import unidecode
 urllib3.disable_warnings()
 
 
-def first_lether(string:str) -> str:
+def first_2lether(string:str) -> str:
 	"""
 	input: Alonson Fernando Gonzales del Rio 
 	output: afgdr
 	"""
 	words_ = string.strip().split(" ")
-	fl = ''.join(word[0].lower() for word in words_)
+	try:
+		fl = ''.join(word[0:2].lower() for word in words_)
+	except:
+		fl = ''.join(word[0:1].lower() for word in words_)
 	return fl
 
 def tidy_text(text : str) -> str:
 	tt = text.lower().strip()
-	unico
-	print(tt)
+	return tt
 
-tidy_text("hola mundO, sóy jññhon")
+def random_author(n = 6, year = [1980, 2023]) -> str:
+	random_string = ''.join(random.choice(string.ascii_letters) for _ in range(n)).lower()
+	year_random = random.randint(year[0], year[1])
+	random_author = f'{random_string}_{year_random}'
+	return random_author
+
+# print(first_2lether('ohoasldf a weoiruasjdfl'))
+# print(random_author())
